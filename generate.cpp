b14d1444a1283923eead3e97e75c3b49d876da4d
T.H. Lin <thlin002@gmail.com>
2020-04-01 23:23:20 +0800

    modified:   generate.cpp 	modified:   sudoku.cpp generate funtion is completed

--- generate.cpp
+++ generate.cpp
@@ -1,5 +1,14 @@
+#include<iostream>
 #include"sudoku.h"
 
 int main(){
+    Sudoku a =  Sudoku::generate();    
+    for(int i = 0; i < 9; ++i){
+        for(int j = 0; j < 9; ++j){
+            cout << a.prob.at(i).at(j);
+            cout << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
--- sudoku.cpp
+++ sudoku.cpp
@@ -1,5 +1,6 @@
 #include"sudoku.h"
-/*
+#include<cstdlib>
+#include<ctime>
 const vector<vector<int> > Sudoku::gen = {{1,2,3,4,5,6,7,8,9},
 				    	  {4,5,6,7,8,9,1,2,3},
 				  	  {7,8,9,1,2,3,4,5,6},
@@ -9,17 +10,29 @@ const vector<vector<int> > Sudoku::gen = {{1,2,3,4,5,6,7,8,9},
 					  {2,3,1,5,6,4,8,9,7},
 					  {5,6,4,8,9,7,2,3,1},
 					  {8,9,7,2,3,1,5,6,4}};
-*/
+
 //Constructor
 Sudoku::Sudoku(){}
+Sudoku::Sudoku(int n){
+
+}
 //Generate
 Sudoku Sudoku::generate(){
+    int a,b;
+    srand(time(NULL));
 	Sudoku sgen;
-	sgen.prob;
+	sgen.prob = sgen.gen;
+    for(int i=0; i<20; ++i){
+        a = (rand()%9);
+        b = (rand()%9);
+        sgen.prob.at(a).at(b) = 0;
+    }
 	return sgen;
 }
 //Transform
-void Sudoku::swapNum(int x, int y){}
+void Sudoku::swapNum(int x, int y){
+    
+}
 void Sudoku::swapRow(int x, int y){}
 void Sudoku::swapCol(int x, int y){}
 void Sudoku::rotate(int x){}
